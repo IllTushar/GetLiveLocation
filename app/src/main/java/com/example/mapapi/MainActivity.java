300eca176d73a2b4e04c852d57a3ecd47680fd8e
IllTushar <gtushar7668@gmail.com>
2023-05-14 06:11:24 -0700

    First Commit

---
+++ app/src/main/java/com/example/mapapi/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.mapapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
+import android.Manifest;
+public class MainActivity extends AppCompatActivity {
+    SupportMapFragment smf;
+    FusedLocationProviderClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        smf = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        Dexter.withContext(getApplicationContext())
+                .withPermissions(
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                        ,Manifest.permission.ACCESS_FINE_LOCATION
+                )
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if (report.areAllPermissionsGranted()) {
+                            getMyLocation();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        // Remember to invoke this method when the custom rationale is closed
+                        // or just by default if you don't want to use any custom rationale.
+                        token.continuePermissionRequest();
+                    }
+                })
+                .withErrorListener(error -> {
+//                    toast(error.name());
+                })
+                .check();
+
+
+    }
+
+    private void getMyLocation() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+              smf.getMapAsync(new OnMapReadyCallback() {
+                  @Override
+                  public void onMapReady(GoogleMap googleMap) {
+                      LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                      MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You are here!!");
+                      googleMap.addMarker(markerOptions);
+                      googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,17));
+
+                  }
+              });
+            }
+        });
+    }
+}
